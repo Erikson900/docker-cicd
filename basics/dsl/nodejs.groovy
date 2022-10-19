6d8e620f3a6007cea7bec3b58ebfdc4aeea8a4ca
Erikson900 <59978862+Erikson900@users.noreply.github.com>
2022-10-19 11:19:05 +0300

    job added

--- basics/dsl/nodejs.groovy
+++ basics/dsl/nodejs.groovy
@@ -17,3 +17,21 @@ job('NodeJS example') {
     }
 }
 
+job('Second Job For Example') {
+    scm {
+        git('git://github.com/wardviaene/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
+            node / gitConfigName('DSL yaniv')
+            node / gitConfigEmail('jenkins-dsl@newtech.academy')
+        }
+    }
+    triggers {
+        scm('H/5 * * * *')
+    }
+    wrappers {
+        nodejs('nodejs') // this is the name of the NodeJS installation in 
+                         // Manage Jenkins -> Configure Tools -> NodeJS Installations -> Name
+    }
+    steps {
+        shell("npm install")
+    }
+}
